3b5140401f74ae62ce3acbe6a6ff586b6b8d8d1d
Colin Flanagan-Morgan <quadvillian@tangerine.local>
2010-07-17 19:09:05 -0400

    build cleanup

---
+++ src/test/scala/cfm/test/ListingsTests.scala
@@ -0,0 +1,27 @@
+import cfm.Listing
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+import org.squeryl.adapters.H2Adapter
+import org.squeryl.{SessionFactory, Session}
+
+class StackSpec extends FlatSpec with ShouldMatchers {
+  "An adaptor" should "initialize an Squerly SessionFactory" in {
+
+    Class.forName("org.h2.Driver");
+    SessionFactory.concreteFactory = Some(() =>
+      Session.create(
+        java.sql.DriverManager.getConnection("jdbc:h2:~/test", "sa", ""),
+        new H2Adapter()))
+
+        SessionFactory.newSession
+  }
+  "A listing object" should "create a schema" in {
+    import org.squeryl.customtypes.CustomTypesMode._
+    import org.squeryl.customtypes._
+    import cfm.Listing._
+    SessionFactory.newSession.bindToCurrentThread
+    Listing.create
+    val restaurants = from(listings)(p => where(p.clazz === "restaurants") select (p))
+  }
+}
--- src/test/scala/cfm/test/RepoTests.scala
+++ src/test/scala/cfm/test/RepoTests.scala
@@ -3,11 +3,13 @@ package cfm
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import cfm.Repo._
-import org.squeryl.adapters.H2Adapter
-import org.squeryl.{SessionFactory, Session}
+
 
 class StackSpec extends FlatSpec with ShouldMatchers {
   "A Repo" should "iterate over nodes" in {
+    val repoHome = "src/test/repository"
+    System.setProperty("org.apache.jackrabbit.repository.home",repoHome)
+    System.setProperty("org.apache.jackrabbit.repository.conf",repoHome + "/repository.xml")
     val r = new TransientRepoAdaptor()
     val s = r.repo.login()
     for (n <- s.getRootNode.getNodes) {
@@ -16,35 +18,7 @@ class StackSpec extends FlatSpec with ShouldMatchers {
     }
     s.logout
   }
-  "An adaptory" should "initialize a SessionFactory" in {
-    import org.squeryl.SessionFactory
-    import org.squeryl.adapters._
-    /*
 
-        Class.forName("com.mysql.jdbc.Driver");
-
-        SessionFactory.concreteFactory = Some(() =>
-          Session.create(
-            java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3308/db"),
-            new MySQLAdapter()))
-    */
-
-    Class.forName("org.h2.Driver");
-    SessionFactory.concreteFactory = Some(() =>
-      Session.create(
-        java.sql.DriverManager.getConnection("jdbc:h2:~/test", "sa", ""),
-        new H2Adapter()))
-
-        SessionFactory.newSession
-  }
-  "A listing object" should "create a schema" in {
-    import org.squeryl.customtypes.CustomTypesMode._
-    import org.squeryl.customtypes._
-    import cfm.Listing._
-    SessionFactory.newSession.bindToCurrentThread
-    Listing.create
-    val restaurants = from(listings)(p => where(p.clazz === "restaurants") select (p))
-  }
 }
 
 
