cbc238f45e470fca6757f07b5cf77f748aa7f0fa
Colin Flanagan <cflanagan@nymag.com>
2011-05-29 20:23:22 -0400

    Skeleton of intellij plugin

---
+++ repobrowser/src/cfm/rb/RBToolManagerFactory.java
@@ -0,0 +1,13 @@
+package cfm.rb;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+
+public class RBToolManagerFactory implements ToolWindowFactory{
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+
+
+    }
+}
