cba0b38b4bb4af929492be820d02f0377b396399
Colin Flanagan <cflanagan@nymag.com>
2011-05-30 15:02:00 -0400

    parsing abilities

--- sandbox/src/main/scala/cfm/search/S.scala
+++ sandbox/src/main/scala/cfm/search/S.scala
@@ -6,6 +6,9 @@ import java.io.{FileFilter, File}
 import org.apache.lucene.index.IndexReader
 import scala.collection.JavaConversions._
 import java.util.{Set => JSet}
+import org.apache.lucene.queryParser.QueryParser
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+
 
 class S(indexDirs: List[String]) {
   val idxs = for {i <- indexDirs;
@@ -16,10 +19,13 @@ class S(indexDirs: List[String]) {
 
   def allFields() = is.foldLeft(Set[String]())((f: Set[String], v: IndexSearcher) => f ++ fields(v))
 
+  def parser(field: String) = new QueryParser(field, new StandardAnalyzer())
   private def fields(is: IndexSearcher): Set[String] = {
     val fs = is.getIndexReader.getFieldNames(IndexReader.FieldOption.ALL).asInstanceOf[JSet[String]]
     fs.foldLeft(Set[String]())((f: Set[String], v: String) => f + v)
   }
+
+  def search(field: String, query: String) = is.map(f => {f.search(parser(field).parse(query),10)})
 }
 
 class SegFilter extends FileFilter {
