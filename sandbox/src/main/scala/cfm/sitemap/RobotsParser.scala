9cfab94a0f0ed2d592645d6aa062db86b75c3818
Colin Flanagan <cflanagan@nymag.com>
2011-06-13 00:19:57 -0400

    starting parser/combinator

---
+++ sandbox/src/main/scala/cfm/sitemap/RobotsParser.scala
@@ -0,0 +1,22 @@
+package cfm.sitemap
+
+import util.parsing.combinator._
+
+class RobotsParser extends JavaTokenParsers{
+//override protected val whiteSpace = """""".r
+
+  def robots: Parser[Any] = sitemap | rule
+  def rule: Parser[Any] = agent~ allow | disallow
+  def sitemap: Parser[Any] = "Sitemap"~":"~"[\\S]+".r
+  def agent: Parser[Any] = "User-agent"~":"~"[\\S]+".r
+  def disallow: Parser[Any] = rep("Disallow"~":"~"[\\S]+".r)
+  def allow: Parser[Any] = rep("Allow"~":"~"[\\S]+".r)
+
+}
+
+object ParseExpr extends RobotsParser {
+  def main(args: Array[String]) {
+    println("input : "+ args(0))
+    println(parseAll(rule, args(0)))
+  }
+}
