8211a066ac5ddbeac38a2eb028d6814b11b22dd6
Colin Flanagan <colin.flanagan@nymag.com>
2011-08-04 09:17:54 -0400

    thingy for the static system nodes

---
+++ sandbox/src/main/java/cfm/jcr/persistence/StaticNodes.java
@@ -0,0 +1,12 @@
+package cfm.jcr.persistence;
+
+public class StaticNodes {
+
+    "cafebabe-cafe-babe-cafe-babecafebabe", "/"
+    "deadbeef-cafe-babe-cafe-babecafebabe", "/jcr:system"
+    "deadbeef-face-babe-cafe-babecafebabe", /"jcr:system/jcr:versionStorage"
+    "deadbeef-cafe-cafe-cafe-babecafebabe", "/jcr:system/jcr:nodeTypes""
+
+
+
+}
